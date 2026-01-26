942b6140f39d691f79aaa625bf0c0c4242192829
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-26 12:03:36 +0900

    Title: 따옴표 출력, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260126/따옴표 출력/print-quote.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String arr="He says \"It\'s a really simple sentece\".";
+        System.out.println(arr);
+
+    }
+}
