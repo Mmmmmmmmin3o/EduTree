bbe224d96a35f6cb8a5ce0be698d4ea5750b0231
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-26 20:52:29 +0900

    Title: 배수의 개수, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 260126/배수의 개수/number-of-multipliers.java
+++ 260126/배수의 개수/number-of-multipliers.java
@@ -10,20 +10,26 @@ public class Main {
 
         for(int i=0; i<10; i++){
             arr[i]=sc.nextInt();
+            if(arr[i]%3==0){
+                answer[0]+=1;
+            }
+            if(arr[i]%5==0){
+                answer[1]+=1;
+            }
         }
 
-        for(int i=0; i<10; i++){
-            if(arr[i]%3==0&&arr[i]%5==0){
-                answer[0]+=1;
-                answer[1]+=1;
-            }
-            else if(arr[i]%3==0){
-                answer[0]+=1;
-            }
-            else if(arr[i]%5==0){
-                answer[1]+=1;
-            }
-        }
+        // for(int i=0; i<10; i++){
+        //     if(arr[i]%3==0&&arr[i]%5==0){
+        //         answer[0]+=1;
+        //         answer[1]+=1;
+        //     }
+        //     else if(arr[i]%3==0){
+        //         answer[0]+=1;
+        //     }
+        //     else if(arr[i]%5==0){
+        //         answer[1]+=1;
+        //     }
+        // }
 
         for(int i=0; i<2; i++){
             System.out.printf("%d ", answer[i]);
