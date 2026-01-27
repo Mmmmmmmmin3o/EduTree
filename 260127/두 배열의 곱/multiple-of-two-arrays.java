72732867496412041832d959d1392280757f07eb
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-27 17:28:00 +0900

    Title: 두 배열의 곱, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260127/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc=new Scanner(System.in);
+
+        int[][] arr=new int[3][3];
+        int[][] arr2=new int[3][3];
+        int[][] answer=new int[3][3];
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                arr2[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                answer[i][j]=arr[i][j]*arr2[i][j];
+                System.out.printf("%d ", answer[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
