3348879b09f2d1a0f42e7a3a344451155ad5cb17
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-27 11:31:14 +0900

    Title: A 부터 B 까지, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260127/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc=new Scanner(System.in);
+
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        while(a<=b){
+            System.out.printf("%d ", a);
+                if(a%2==0){
+                    a+=3;
+                }
+                else{
+                    a*=2;
+                }
+        }
+
+    }
+}
