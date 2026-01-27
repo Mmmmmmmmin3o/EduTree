51ac70c7bbdf9116c366823d1e704d559add98e4
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-27 18:48:46 +0900

    Title: 특정 원소들의 합, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260127/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc=new Scanner(System.in);
+        int[][] arr=new int[4][4];
+        int answer=0;
+
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<4; i++){
+            for(int j=0; j<i+1; j++){
+                answer+= arr[i][j];
+            }
+        }
+        System.out.println(answer);
+    }
+}
