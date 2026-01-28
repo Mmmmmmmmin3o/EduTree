44e8d3596a594f5b2977d1e81afffae3cf818d0a
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-28 15:28:55 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260128/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weight=13;
+        int gravity=0.165;
+
+        System.out.printf("%d * %d = %d",weight,gravity,weight*gravity);
+    }
+}
