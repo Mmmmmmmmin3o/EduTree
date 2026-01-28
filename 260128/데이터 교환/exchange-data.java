c905d1fd5168cc82bbd1966112b143a1f381b08d
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-28 15:46:28 +0900

    Title: 데이터 교환, Time: 97ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260128/데이터 교환/exchange-data.java
+++ 260128/데이터 교환/exchange-data.java
@@ -5,12 +5,11 @@ public class Main {
         int b=6;
         int c=7;
 
-        int temp=b;
-        int temp2=c;
+        int temp=a;
 
+        a=c;
+        c=b;
         b=a;
-        c=temp;
-        a=temp2;
 
         System.out.printf("%d\n%d\n%d",a,b,c);
     }
