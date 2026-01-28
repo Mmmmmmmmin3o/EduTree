f0b91782657c35decf17967d3eb8271863d78aba
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-28 15:33:10 +0900

    Title: 소수점 반올림하기, Time: 100ms, Memory: 9MB, Status: Passed - Codetree

--- 260128/소수점 반올림하기/rounding-decimal-points.java
+++ 260128/소수점 반올림하기/rounding-decimal-points.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        dobule a=25.352;
+        double a=25.352;
 
         System.out.printf("%.1f",a);
     }
