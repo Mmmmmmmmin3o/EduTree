6ceaf764034f037bc08f109bd7cd88cdcbd6572b
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-28 15:59:50 +0900

    Title: 입력받아 계산 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260128/입력받아 계산 2/input-calculate-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        System.out.println(a*b);
+    }
+}
