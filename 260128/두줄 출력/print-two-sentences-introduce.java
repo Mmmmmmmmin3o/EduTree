fa4fa76f2912205193b12180dffff03b2b285335
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-28 15:19:32 +0900

    Title: 두줄 출력, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 260128/두줄 출력/print-two-sentences-introduce.java
+++ 260128/두줄 출력/print-two-sentences-introduce.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.printf("Hello students!\nWelcome to LeebrosCode!")
+        System.out.printf("Hello students!\nWelcome to LeebrosCode!");
     }
 }
