fe8d438b291b957b28ecaf3e4e0bc39c398971a0
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-01-28 15:57:40 +0900

    Title: 길이 단위 환산하기, Time: 145ms, Memory: 11MB, Status: Passed - Codetree

--- 260128/길이 단위 환산하기/convert-length-units.java
+++ 260128/길이 단위 환산하기/convert-length-units.java
@@ -5,7 +5,7 @@ public class Main {
         // Please write your code here.
         Scanner sc=new Scanner(System.in);
 
-        double N=sc.nextFloat();
+        double N=sc.nextDouble();
         double ft=30.48;
 
         System.out.printf("%.1f",N*ft);
