e0b95ab83e34a1e58b7ff942cb15ac1314a8b15e
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-23 10:36:56 +0900

    Title: 두수의 곱과 몫, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+
+    cin>>a>>b;
+
+    int div=a/b;
+    int mul=a*b;
+
+    cout<<a<<" * "<<b<<" = "<<mul<<endl;
+    cout<<a<<" / "<<b<<" = "<<div;
+
+    return 0;
+}
