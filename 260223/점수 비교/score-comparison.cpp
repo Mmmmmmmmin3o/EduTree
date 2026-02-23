e144483dcf7f53a5e9af09f0abec0ffb97f5eea7
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-23 20:08:10 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/점수 비교/score-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int mA,eA,mB,eB;
+    cin>>mA>>eA>>mB>>eB;
+
+    (mA>mB)and(eA>eB)?cout<<1:cout<<0;
+    return 0;
+}
