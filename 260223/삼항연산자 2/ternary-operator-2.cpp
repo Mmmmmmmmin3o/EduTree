bc7fcf363e5ecf48323b36bbd164cf0cac902ca7
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-23 18:47:59 +0900

    Title: 삼항연산자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+
+    cin>>a;
+
+    string answer=(a==1)?"t":"f";
+
+    cout<<answer;
+    return 0;
+}
