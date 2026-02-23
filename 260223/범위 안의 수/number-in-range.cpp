aa16dd3c52fbbd2bb299ddb7256f89a61103d8a7
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-23 20:09:47 +0900

    Title: 범위 안의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    (a>=10)and(a<=20)?cout<<"yes":cout<<"no";
+    return 0;
+}
