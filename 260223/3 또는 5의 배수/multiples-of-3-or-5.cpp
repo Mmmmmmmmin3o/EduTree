871770b16ce25f3efe95cf2293ba8117f923f559
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-23 19:50:37 +0900

    Title: 3 또는 5의 배수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    (a%3==0)?cout<<"YES":cout<<"NO";
+    cout<<endl;
+    (a%5==0)?cout<<"YES":cout<<"NO";
+    return 0;
+}
