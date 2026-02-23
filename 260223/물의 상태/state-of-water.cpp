ce5aa07b1fca454b15cd4b32fecd27eeffbf74c6
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-23 18:50:38 +0900

    Title: 물의 상태, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/물의 상태/state-of-water.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int temp;
+
+    cin>>temp;
+
+    if(temp<0){
+        cout<<"ice";
+    }
+    else if(temp>99){
+        cout<<"vapor";
+    }
+    else{
+        cout<<"water";
+    }
+    return 0;
+}
