1948753aa6af7150e36f3344a0881bb2b25040e0
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-23 11:06:29 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260223/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+
+    cin>>a>>b;
+
+    int add =a+b;
+    float avg=add/2;
+
+    printf("%d %.1f",add,avg);
+    return 0;
+}
