c6d5ba4e92ae688aa60add607bbf746354eb2156
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-24 15:55:39 +0900

    Title: 짝수만 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260224/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int b,a;
+
+    cin>>b>>a;
+
+    while(b>a-1){
+        if(b%2==0){
+            cout<<b<<" ";
+        }
+        b--;
+    }
+    return 0;
+}
