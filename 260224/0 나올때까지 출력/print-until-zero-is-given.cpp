927c193a87b7d16b69844464b51b0bdefa0c0f2e
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-24 20:48:35 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260224/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while(true){
+        int temp;
+        cin>>temp;
+
+        if(temp==0){
+            break;
+        }
+        else{
+            cout<<temp<<endl;
+        }
+    }
+    return 0;
+}
