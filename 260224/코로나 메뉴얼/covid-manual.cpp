b2ed24053bfe1d775c661b4fac711334d7a4c611
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-24 15:07:03 +0900

    Title: 코로나 메뉴얼, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260224/코로나 메뉴얼/covid-manual.cpp
+++ 260224/코로나 메뉴얼/covid-manual.cpp
@@ -6,20 +6,22 @@ int main() {
     char a,b,c;
     int temp_a,temp_b,temp_c;
 
-    char answer='N';
+    int cnt=0;
 
     cin>>a>>temp_a>>b>>temp_b>>c>>temp_c;
 
     if(a=='Y' and temp_a>=37){
-        if(b=='Y' and temp_b>=37){
-            answer='E';
-        }
-        if(c=='Y' and temp_c>=37){
-            answer='E';
-        }
+        cnt++;
     }
-
-    cout<<answer;
+    if(b=='Y' and temp_b>=37){
+        cnt++;
+    }
+    if(c=='Y' and temp_c>=37){
+        cnt++;
+    }
+    
+    if(cnt>1) cout<<'E';
+    else cout<<'N';
     
     return 0;
 }
