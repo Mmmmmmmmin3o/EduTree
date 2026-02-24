cfed0222173aaa2696172182ee087665fff343bf
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-24 14:25:47 +0900

    Title: 장학금, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260224/장학금/scholarship.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int mid,last;
+
+    cin>>mid>>last;
+
+    if(mid>=90){
+        if(last>=95){
+            cout<<100000;
+        }
+        else if(last>=90){
+            cout<<50000;
+        }
+        else{
+            cout<<0;
+        }
+    }
+    else{
+        cout<<0;
+    }
+
+    return 0;
+
+}
