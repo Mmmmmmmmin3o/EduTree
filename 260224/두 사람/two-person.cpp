249db4a10588c53819d7e133717adc46511d2c94
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-24 14:42:10 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260224/두 사람/two-person.cpp
+++ 260224/두 사람/two-person.cpp
@@ -8,9 +8,8 @@ int main() {
 
     cin>>age1>>sex1>>age2>>sex2;
 
-    if(age1>=19 || age2>=19){
-        if(sex1=='M' || sex2=='M') cout<<1;
-        else cout<<0;
+    if((sex1=='M' and age1>=19) || (age2>=19 and sex2=='M')){
+        cout<<1;
     }
     else cout<<0;
     return 0;
