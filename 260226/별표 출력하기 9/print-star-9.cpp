23d66fbb67fefbd53d924ef2cdb19fdd69a963b9
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-26 17:45:23 +0900

    Title: 별표 출력하기 9, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260226/별표 출력하기 9/print-star-9.cpp
+++ 260226/별표 출력하기 9/print-star-9.cpp
@@ -6,14 +6,21 @@ int main() {
     int n;
     cin>>n;
 
-    int row=n+2;
+    // for(int i=0; i<n; i++){ //n번 반복
+    //     for(int j=n-i; j>0; j--){ //n-i만큼 띄어쓰기
+    //         cout<<" ";
+    //     }
+    //     for(int j=0; j<2*i+1; j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
 
-    for(int i=0; i<n; i++){
-        for(int x=row; x>0; x--){
+    for(int i=n; i>0; i--){
+        for(int j=i*2-1; j>1; j--){
             cout<<" ";
         }
-        row-=2;
-        for(int j=0; j<=2*i; j++){
+        for(int j=0; j<2*(n-i)+1; j++){
             cout<<"* ";
         }
         cout<<endl;
