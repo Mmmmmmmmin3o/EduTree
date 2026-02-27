f7507e7291601ca094f7d6b9544b531b379b5cbb
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-02-27 13:52:09 +0900

    Title: 별 그리기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260227/별 그리기/star-drawing.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+
+    for(int i=1;i<n+1; i++){
+        for(int j=n-i; j>0; j--) cout<<" ";
+        for(int j=0; j<2*i-1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    for(int i=n-1; i>0; i--){
+        for(int j=n-i; j>0; j--) cout<<" ";
+        for(int j=0; j<2*i-1; j++) cout<<"*";
+        cout<<endl;
+    }
+
+    return 0;
+}
