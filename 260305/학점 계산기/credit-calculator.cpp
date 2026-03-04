6ef9802b437bf67c0fba6bf7291beea7a2b641e1
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-03-05 06:30:19 +0900

    Title: 학점 계산기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260305/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+float sum=0;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    float grade[n];
+
+    for(int i=0; i<n; i++){
+        cin>>grade[i];
+        sum+=grade[i];
+    }
+
+    float avg=sum/n;
+
+    printf("%.1f\n",avg);
+
+    avg>3.9?cout<<"Perfect":(avg>2.9?cout<<"Good":cout<<"poor");
+    return 0;
+}
