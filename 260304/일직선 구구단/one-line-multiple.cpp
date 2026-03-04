80d306df114e15609320f8fcb2ce1706a4c9ebfe
Mmmmmmmmin3o <140192491+Mmmmmmmmin3o@users.noreply.github.com>
2026-03-04 12:47:56 +0900

    Title: 일직선 구구단, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260304/일직선 구구단/one-line-multiple.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    for(int i=1; i<n+1; i++){
+        for(int j=1; j<n+1;j++){
+            printf("%d * %d = %d", i, j, i*j);
+            cout<<endl;
+        }
+    }
+    return 0;
+}
